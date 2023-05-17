d0fd3a3dfe40221730b75c42a0262a3a0c8262c0
end-dot <aj.1803160@gmail.com>
2023-05-17 12:50:03 +0600

    day4 completed

--- my_app/hello/lib/main.dart
+++ my_app/hello/lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 void main(){
   runApp(MyApp());
 
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    int days=30;
+    String name="Jim";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello world!"),
-          ),
-        ),
-      ),
+      home: HomePage(),
 
     );
+   
   }
 }
